d3514077fcfa551bb73ad40095c34232e9b15228
tonmoy <tonmoy3673@gmail.com>
2022-11-09 01:34:44 +0600

    set json data

--- index.js
+++ index.js
@@ -12,14 +12,27 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5fuqo8h.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+ async function run(){
+
+    try{
+        const serviceCollection=client.db('healthCoach').collection('services');
+        
+        app.get('/services',async(req,res)=>{
+            const query={}
+            const cursor=serviceCollection.find(query);
+            const services=await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+ }
+
+ run().catch(err=>console.error(err));
 
 
 
