1433354c3cab2cd17837d54dc3ac56007c06a698
tonmoy <tonmoy3673@gmail.com>
2022-11-10 01:51:40 +0600

    set find one api link

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors=require ('cors');
 const app=express();
 require('dotenv').config();
 const port=-process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
@@ -31,6 +31,14 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
             const cursor=serviceCollection.find(query);
             const services=await cursor.limit(3).toArray();
             res.send(services);
+        })
+
+        app.get('/services/:id',async(req,res)=>{
+
+            const id=req.params.id;
+            const query={_id:ObjectId(id)};
+            const service=await serviceCollection.findOne(query);
+            res.send(service);
         })
     }
     finally{
